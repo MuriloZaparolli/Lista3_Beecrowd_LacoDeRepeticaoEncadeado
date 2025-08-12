e6ea7c4f74864d1abbcebc90d000710eead68d2a
Murilo de Lima Zaparolli <muzapa10.mzp10@gmail.com>
2025-08-12 15:04:23 -0300

    Create SequenciaIJ1.java

---
+++ SequenciaIJ1.java
@@ -0,0 +1,10 @@
+public class SequenciaIJ1 {
+    public static void main(String[] args) {
+        int j=60, i=1;
+        while (j>=0) {
+            System.out.printf("I=%d J=%d\n", i, j);
+            i+=3;
+            j-=5;
+        }
+    }
+}
